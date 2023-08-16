5a1eba1c2d95d2bfb84a21ff43daf902a3a0df28
sowmi-1234 <142321993+sowmi-1234@users.noreply.github.com>
2023-08-16 18:44:29 +0530

    Add files via upload

---
+++ MyAssignments/CheckPositiveorNegative.java
@@ -0,0 +1,23 @@
+package seleninumSample;
+
+public class CheckPositiveorNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num=123;
+        if(num>0)
+        {
+         System.out.println("The number is Positive");
+        }
+        else if(num<0)
+        {
+        	System.out.println("The number is Negative");
+        	
+        }
+        else 
+        { 
+        	System.out.println("The number is zero");
+        }
+	}
+
+}
