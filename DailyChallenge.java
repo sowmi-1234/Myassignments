c6fd7ade79928806179f81f30088d92182dfed29
sowmi-1234 <142321993+sowmi-1234@users.noreply.github.com>
2023-08-28 19:47:37 +0530

    Add files via upload

---
+++ DailyChallenge.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+public class DailyChallenge {
+  
+		   public static void main(String[] args) {
+		        String str = "Hello World";
+		        String strlen = str.substring(6);
+		        System.out.println("The length of last word is " + strlen.length());
+		        String str1 =" fly me   to   the moon";
+		        String str1len = str1.substring(19);
+		        System.out.println("The length of last word is " +str1len.length() );
+		        String str2= "luffy is still joyboy";
+		        String str2len = str2.substring(15);
+		        System.out.println("The length of last word is " +str2len.length());
+		        
+		        		
+		    }
+
+				}
+		
+
+
+
+
+
+
+
+	
+
+
